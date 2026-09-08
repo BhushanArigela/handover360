cc1d141441be9f551709d0140f39b654412fb57d
BhushanArigela <32087680+BhushanArigela@users.noreply.github.com>
2026-09-08 19:05:37 +0530

    Add files via upload

--- src/components/engineer/EngineerReviews.tsx
+++ src/components/engineer/EngineerReviews.tsx
@@ -380,7 +380,7 @@ console.log("Responses:", data.responses);
                           {resp.media.map((m:any)=>(
                             <img
                             key={m.media_id}
-                            src={`${BASE_URL}${m.file}`}
+                            src={`${m.file}`}
                             className="w-24 h-24 rounded object-cover"
                             />
                           ))}
