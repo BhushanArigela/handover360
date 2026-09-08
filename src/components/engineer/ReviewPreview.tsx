6131dece858fbb33a582a410e61dbdff92261aa9
BhushanArigela <32087680+BhushanArigela@users.noreply.github.com>
2026-09-08 19:05:07 +0530

    Add files via upload

--- src/components/engineer/ReviewPreview.tsx
+++ src/components/engineer/ReviewPreview.tsx
@@ -9,7 +9,6 @@ import {
     ChevronRight, Building2
 } from "lucide-react";
 
-import { BASE_URL } from '../../config/env';
 
 interface Props {
     open: boolean;
@@ -311,8 +310,8 @@ export const ReviewPreview: React.FC<Props> = ({
                                                                                 <img
                                                                                     key={photo.id}
                                                                                     className="cursor-pointer rounded-lg border h-30 w-full object-cover"
-                                                                                    src={`${BASE_URL}${photo.file}`}
-                                                                                    onClick={() => window.open(`${BASE_URL}${photo.file}`, "_blank")}
+                                                                                    src={`${photo.file}`}
+                                                                                    onClick={() => window.open(`${photo.file}`, "_blank")}
                                                                                     alt=""
                                                                                     
                                                                                     
